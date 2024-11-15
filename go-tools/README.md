adf49a57cad6fb34da0650fe43ee725fbc94ba60
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2024-11-15 15:57:50 +0800

    add changes

---
+++ go-tools/README.md
@@ -0,0 +1,10 @@
+# go-tools
+
+## Requirements
+```
+# Install go
+https://go.dev/doc/install
+
+# Install Task
+https://github.com/go-task/task/releases
+```
--- go-tools/container-mirror/cmd/main/main.go
+++ go-tools/container-mirror/cmd/main/main.go
@@ -1,30 +1,75 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"flag"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/containers/image/v5/docker"
+	"github.com/containers/image/v5/types"
 )
 
-func main() {
-	ref, err := docker.ParseReference("//fedora")
-	if err != nil {
-		panic(err)
-	}
-
-	ctx := context.Background()
-
+func downloadImage(ctx context.Context, ref types.ImageReference) error {
 	img, err := ref.NewImage(ctx, nil)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("creating image: %w", err) // Wrap errors for better context
 	}
+	defer img.Close() // Crucial for resource cleanup
+	// ... (rest of download logic)
+	return nil
+}
 
-	defer img.Close()
-	b, _, err := img.Manifest(ctx)
+func main() {
+	imageListPathPtr := flag.String("imagelist-path", "sample-imagelist", "Text file of all container images on each line.")
+	log.Println("imagelist-path: ", *imageListPathPtr)
+
+	concurrencyPtr := flag.Int("concurrency", 1, "Number of simultaneous downloads.")
+	log.Println("concurrency:", *concurrencyPtr)
+
+	imagelist_file, err := os.Open(*imageListPathPtr)
 	if err != nil {
 		panic(err)
 	}
+	defer imagelist_file.Close()
 
-	fmt.Printf("%s", string(b))
+	log.Println("Validating ImageList contents...")
+	imagelist_scanner := bufio.NewScanner(imagelist_file)
+	for imagelist_scanner.Scan() {
+		image_ref := imagelist_scanner.Text()
+		log.Println(image_ref)
+		_, err := docker.ParseReference("//" + image_ref)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	imagelist_file.Seek(0, 0)
+	imagelist_scanner = bufio.NewScanner(imagelist_file)
+	for imagelist_scanner.Scan() {
+		image_ref := imagelist_scanner.Text()
+		log.Println("Downloading image: " + image_ref)
+		log.Println(image_ref)
+		ref, err := docker.ParseReference("//" + image_ref)
+		if err != nil {
+			panic(err)
+		}
+
+		ctx := context.Background()
+		img, err := ref.NewImage(ctx, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		defer img.Close()
+		b, _, err := img.Manifest(ctx)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("%s", string(b))
+	}
+
 }
